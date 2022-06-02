48fde1e3de7ca2df3c1348e2a1848254ec49364c
Olatunji Sobayo <sobayolatunji@gmail.com>
2022-06-02 14:11:52 -0700

    next level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main - main block
-*Return : 0
-*/
-int main(block)
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
