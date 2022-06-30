244bacf68fd946e2f966ecf9a35a8bd2aa38765f
Olatunji Sobayo <sobayolatunji@gmail.com>
2022-06-30 12:39:49 -0700

    Trust no man

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,12 +56,12 @@ int main(int argc, char *argv[])
 }
 
 /**
-* _isnumber - checks for digit-only (0 through 9) numbers
-*
-* @str: parameter hard-coded in main
-*
-* Return: 1 or 0
-*/
+ * _isnumber - checks for digit-only (0 through 9) numbers
+ *
+ * @str: parameter hard-coded in main
+ *
+ * Return: 1 or 0
+ */
 
 int _isnumber(char *str)
 {
@@ -76,9 +76,9 @@ int _isnumber(char *str)
 }
 
 /**
-* _error - print error
-* Return: void
-*/
+ * _error - print error
+ * Return: void
+ */
 
 void _error(void)
 {
@@ -91,12 +91,12 @@ void _error(void)
 }
 
 /**
-* _strlen - function that returns the length of a string
-*
-* @s: parameter defined in main
-*
-* Return: length of string
-*/
+ * _strlen - function that returns the length of a string
+ *
+ * @s: parameter defined in main
+ *
+ * Return: length of string
+ */
 
 int _strlen(char *s)
 {
@@ -111,11 +111,11 @@ int _strlen(char *s)
 }
 
 /**
-* _calloc - function that allocates memory for an array, using malloc
-* @nmemb: size of the memory space to allocate in bytes
-* @size: size of type
-* Return: void pointer
-*/
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: size of the memory space to allocate in bytes
+ * @size: size of type
+ * Return: void pointer
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -134,14 +134,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 }
 
 /**
-* _memset - function that fills memory with a constant byte
-*
-* @s: parameter defined in main, pointer to memory area
-* @b: parameter defined in main, constant byte
-* @n: parameter defined in main, number of bytes to be filled
-*
-* Return: memory address of function (memory area)
-*/
+ * _memset - function that fills memory with a constant byte
+ *
+ * @s: parameter defined in main, pointer to memory area
+ * @b: parameter defined in main, constant byte
+ * @n: parameter defined in main, number of bytes to be filled
+ *
+ * Return: memory address of function (memory area)
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
