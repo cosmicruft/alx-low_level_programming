a41ffe9cf4d4d3682ce1addd6848f0ef62771518
Olatunji Sobayo <sobayolatunji@gmail.com>
2022-07-04 06:57:35 -0700

    new

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -10,3 +10,16 @@ int main(void)
 printf("%s\n", _FILE_);
 return (0);
 }
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - name of a file
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
+
